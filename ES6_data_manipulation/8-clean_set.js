6741a8dfb0aa1a232b5879c05d02b5437a9f1cbd
noura <noura.fahad2022@gmail.com>
2026-09-23 17:18:56 +0300

    Handle invalid values in cleanSet

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
 
   const values = [];
 
   for (const value of set) {
-    if (value.startsWith(startString)) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       values.push(value.slice(startString.length));
     }
   }
