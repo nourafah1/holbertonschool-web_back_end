3acc68d71ebac478e73f52762874900fbdb23c06
noura <noura.fahad2022@gmail.com>
2026-09-23 17:41:28 +0300

    Handle multiple settled promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,8 +2,20 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
+  const promises = [
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ];
+
+  return Promise.all(
+    promises.map((promise) => promise
+      .then((value) => ({
+        status: 'fulfilled',
+        value,
+      }))
+      .catch((error) => ({
+        status: 'rejected',
+        value: String(error),
+      }))),
+  );
 }
